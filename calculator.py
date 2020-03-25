68463a39e7c16589759c8e1022befe55b63fd375
anupam-newgen <anupam.v83@gmail.com>
2020-03-25 20:06:54 +0530

    Basics of Python

---
+++ calculator.py
@@ -0,0 +1,8 @@
+print('Add 2 with 2  = ', (2 + 2))
+print('Subtract 2 from 2 = ', (2 - 2))
+print('Multiply 2 with 2  = ', (2 * 2))
+print('2 raise to the power 2  = ', (2 ** 2))
+print('2 divide by 2 = ', (2 / 2))
+
+# This is a comment.
+# You can use above concept to solve complex equations as well.
